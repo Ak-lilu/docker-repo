95c91fc7a018cc8c5e5767d8bd628080dcc2b8c4
Aklilu Weldemihret Weldu <akliluweldu@gmail.com>
2023-10-24 21:26:32 +0200

    scoped namespaces

--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -1,13 +1,10 @@
 #nullable disable
-using System;
-using System.Collections.Generic;
-namespace myWebApp.Models
+namespace myWebApp.Models;
+
+public class Student
 {
-  public class Student
-  {
-    public int ID { get; set; }
-    public string LastName { get; set; }
-    public string FirstMidName { get; set; }
-    public DateTime EnrollmentDate { get; set; }
-  }
+public int ID { get; set; }
+public string LastName { get; set; }
+public string FirstMidName { get; set; }
+public DateTime EnrollmentDate { get; set; }
 }
